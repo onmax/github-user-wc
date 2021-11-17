735101815c502b0651ce28202301fb1788262b0f
Max <onmax@users.noreply.github.com>
2021-11-17 14:44:40 +0000

    building for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
-import html from '@web/rollup-plugin-html';
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   build: {
     lib: {
       entry: 'src/github-user.ts',
@@ -13,23 +10,11 @@ export default defineConfig({
       formats: ['es']
     },
     rollupOptions: {
-      external: /^lit/
-    }
+      external: mode === "production" ? "" : /^lit-element/,
+    },
   },
   plugins: [
-    // Entry point for application build; can specify a glob to build multiple
-    // HTML files for non-SPA app
-    html({
-      input: 'index.html',
-    }),
     // Resolve bare module specifiers to relative paths
     resolve(),
-    // Minify HTML template literals
-    minifyHTML(),
-    // Minify JS
-    terser({
-      ecma: 2020,
-      module: true,
-    }),
   ],
-})
+}))
